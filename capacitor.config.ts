4954b57cdbf90e32c6fcf215c3def329a420d263
IvanNunez88 <ivanezf@gmail.com>
2025-10-21 19:37:59 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'appRentas',
+  webDir: 'www'
+};
+
+export default config;
