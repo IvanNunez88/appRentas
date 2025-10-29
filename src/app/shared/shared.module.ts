6463dcf58ca30a3eb1e576303737100deb362398
IvanNunez88 <ivanezf@gmail.com>
2025-10-28 18:45:46 -0600

    Mejoras 28102025 Parte 1

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { IonAlert, IonButton } from '@ionic/angular/standalone';
+
+@NgModule({
+  declarations: [],
+  imports: [IonButton, IonAlert],
+  exports: [IonButton, IonAlert],
+  providers: [],
+})
+export class SharedModule {}
